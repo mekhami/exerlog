b05795112f385307fd253fde1ad1db84601363c0
Lawrence Vanderpool <lawrence.vanderpool@lab7.io>
2017-12-23 11:26:35 -0600

    add some db migrations and assorted stuff

--- exerlog/workouts/models.py
+++ exerlog/workouts/models.py
@@ -1,15 +1,18 @@
+from django.conf import settings
 from django.db import models
+
 
 # Create your models here.
 class Workout(models.Model):
     name = models.CharField(max_length=256)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 class ExerciseDefinition(models.Model):
     name = models.CharField(max_length=256)
     notes = models.TextField()
 
 class ExerciseInstance(models.Model):
-    definition = models.ForeignKey(ExerciseDefinition)
+    definition = models.ForeignKey(ExerciseDefinition, on_delete=models.CASCADE)
     weight = models.IntegerField()
     date = models.DateField(auto_now_add=True)
     notes = models.TextField()
